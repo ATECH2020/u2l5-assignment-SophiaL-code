cdf3a026c8ff9dc817fc04ec7915e0c480f97050
Sophia Lindsey <sophia.2312279@nv.ccsd.net>
2020-09-27 22:01:26 +0000

    Start of how far away

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,9 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+        double startLat, startLong, endLat, endLong;
+        Scanner scanner = new Scanner(System.in);
+        startLat = scanner.nextDouble();
+        System.out.println("print it " + startLat);
     }
 }
