91b94ca522cb25c3f247e60f2f65822849560255
Sophia Lindsey <sophia.2312279@nv.ccsd.net>
2020-09-27 21:50:08 +0000

    Finished Construction Costs

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -10,14 +10,18 @@ public class ConstructionTester
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the sales tax rate: ");
+        System.out.println("Enter the sales tax rate: ");
         taxRate = scanner.nextDouble();
         System.out.println("How many boards do you need? ");
         numBoards = scanner.nextInt();
         System.out.println("How many windows do you need? ");
         numWindows = scanner.nextInt();
-        
-        Constuction myConstruction = new Construction(8.0, 11.0, taxRate);
+
+        Construction myConstruction = new Construction(8.0, 11.0, taxRate);
+        double total = myConstruction.lumberCost(numBoards) + myConstruction.windowCost(numWindows);
+        double gTotal = myConstruction.grandTotal(total);
+        System.out.println("Total: " + total);
+        System.out.println("Grand total: " + gTotal);
         
     }
 }
